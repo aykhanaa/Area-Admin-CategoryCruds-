97c69649f50db9f716b232b38544c039a1144fed
unknown <aykhanaa@code.edu.az>
2024-05-20 08:59:08 +0400

    create academywork

---
+++ Fiorello-PB101/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Fiorello_PB101.Data;
+using Fiorello_PB101.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiorello_PB101.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public async Task< IActionResult> Index()
+        {
+            return View(await _categoryService.GetAllWithProductCountAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Fiorello-PB101/Services/CategoryService.cs
@@ -0,0 +1,34 @@
+﻿using Fiorello_PB101.Data;
+using Fiorello_PB101.Models;
+using Fiorello_PB101.Services.Interfaces;
+using Fiorello_PB101.ViewModels.Categories;
+using Microsoft.EntityFrameworkCore;
+using System.Drawing.Design;
+
+namespace Fiorello_PB101.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly AppDbContext _context;
+        public CategoryService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<IEnumerable<CategoryProductVM>> GetAllWithProductCountAsync()
+        {
+            IEnumerable<Category> categories = await _context.Categories.Include(m=>m.Products)
+                                                                        .ToListAsync();
+            return categories.Select(m => new CategoryProductVM
+            {
+                CategoryName = m.Name,
+                CreatedDate = m.CreatedDate.ToString("MM.dd.yyyy"),
+                ProductCount = m.Products.Count
+            });
+        }
+    }
+}
